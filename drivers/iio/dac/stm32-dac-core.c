6c7d7aa41343c5cb432cd398bfe1f021594f9b7b
Mark Carlin <mcarlin@datumsystems.com>
2026-02-20 14:16:25 -0700

    Use stm32-dac ch2 unbuffered.

--- drivers/iio/dac/stm32-dac-core.c
+++ drivers/iio/dac/stm32-dac-core.c
@@ -256,7 +256,22 @@ static struct platform_driver stm32_dac_driver = {
 		.pm = pm_ptr(&stm32_dac_core_pm_ops),
 	},
 };
-module_platform_driver(stm32_dac_driver);
+//module_platform_driver(stm32_dac_driver);
+
+/*
+ * DAC driver register needs to be done after ADC for Datum
+ */
+static int __init stm32_dac_drv_reg(void)
+{
+	return platform_driver_register(&stm32_dac_driver);
+}
+late_initcall(stm32_dac_drv_reg);
+
+static void __exit stm32_dac_exit(void)
+{
+	platform_driver_unregister(&stm32_dac_driver);
+}
+module_exit(stm32_dac_exit);
 
 MODULE_AUTHOR("Fabrice Gasnier <fabrice.gasnier@st.com>");
 MODULE_DESCRIPTION("STMicroelectronics STM32 DAC core driver");
